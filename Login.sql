25ea2f962771a9b6ea69c7b13a7c27210f44fc89
Rajalaxmi-M <54747782+Rajalaxmi-M@users.noreply.github.com>
2020-05-05 01:32:29 +0700

    Update Login.sql

--- Login.sql
+++ Login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 04, 2020 at 09:51 AM
+-- Generation Time: May 04, 2020 at 07:03 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Login`
+-- Table structure for table `login`
 --
 
 CREATE TABLE `login` (
@@ -33,25 +33,28 @@ CREATE TABLE `login` (
   `username` varchar(30) NOT NULL,
   `password` varchar(100) NOT NULL,
   `role` int(1) NOT NULL,
-  `status` int(1) NOT NULL
+  `status` int(1) NOT NULL,
+  `dept` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `Login`
+-- Dumping data for table `login`
 --
 
-INSERT INTO `Login` (`id`, `username`, `password`, `role`, `status`) VALUES
-('1111', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, 1),
-('T3540', 'M.Ravindran', 'f024941c0816066194a7ac288efa7aaa', 0, 1);
+INSERT INTO `login` (`id`, `username`, `password`, `role`, `status`, `dept`) VALUES
+('1111', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, 1, 'cse'),
+('T1234', 'Ravi', '6b7f29a4feeb2bcf8d9de882d2030a5e', 0, 1, 'ece'),
+('T2345', 'Raji', 'b1648ecee4f906defb912a1e891a443f', 0, 1, 'it'),
+('T3456', 'Yogam', '1a22316599370b5e322a01015fb62d03', 0, 1, 'CSE');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `Login`
+-- Indexes for table `login`
 --
-ALTER TABLE `Login`
+ALTER TABLE `login`
   ADD PRIMARY KEY (`id`);
 COMMIT;
 
