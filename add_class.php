106893c3e559ee4d15cfa18ac92646b88387d41d
Rajalaxmi-M <54747782+Rajalaxmi-M@users.noreply.github.com>
2020-05-06 00:34:13 +0700

    Update add_class.php

--- add_class.php
+++ add_class.php
@@ -1,7 +1,7 @@
 <?php
 require("connectdb.php");
 require("header.php");
-	if($_SESSION["Role"] == '0')
+	if($_SESSION["role"] == '0')
 {
 	echo "<center><br><br><div class='msg'>You are not authorized to access this page</div></center>";
 	die();
@@ -20,7 +20,7 @@ require("header.php");
 <option value="cse" >CSE</option>
 <option value="ece">ECE</option>
 <option value="it">IT</option>
-<option value="eie">EIE</option>
+
 </select>
 <select name="year" class="form-control" style="margin-top:13px;width:270px;">
 <option value="first_year" >First Year</option>
@@ -41,14 +41,66 @@ require("header.php");
 
 </div>
 <?php
-
+//include('msg.php');
+$new=array("first_year_a_cse","first_year_b_cse","second_year_a_cse","second_year_b_cse",
+"third_year_a_cse","third_year_b_cse","fourth_year_a_cse","fourth_year_b_cse,
+first_year_a_ece","first_year_b_ece","second_year_a_ece","second_year_b_ece",
+"third_year_a_ece","third_year_b_ece","fourth_year_a_ece","fourth_year_b_ece",
+"first_year_a_it","second_year_a_it","third_year_a_it","fourth_year_a_it"
+);
+$x="show tables";
+$y=mysqli_query($con,$x);
+if(mysqli_num_rows($y) > 0)
+	{
+		$flag =1;
+	while($row=mysqli_fetch_array($y))
+	{
+	if(in_array($row[0],$new))
+		{
+			if($flag == 1)
+			{
+				echo "<div class='container'>
+	<div class='row'>
+	<div class='col-md-3'></div>
+	<div class='col-md-6'>
+<center><div class='h'>Class Details</div></center>
+	<table style='margin-top:13px' border=1 class='table table-bordered '>";		
+	echo"<tr class='t'>
+		<th>Class_Name </th>
+		<th>Section </th>
+		<th>Department </th>
+		
+		<th>Remove_Student</th>
+		</tr>";
+			}
+			
+			echo "<tr>";
+		$b=explode("_",$row[0]);
+		echo "<td>$b[0]_$b[1]</td>";
+		echo "<td>$b[2]</td>";
+		echo "<td>$b[3]</td>";
+		//<th>View_Student</th>echo "<td><a style='text-decoration:none' href=stud_view.php?classname=$b[0]_$b[1]_$b[2]_$b[3]>View</a></td>";
+		echo "<td><a style='text-decoration:none' href=c_remove.php?classname=$b[0]_$b[1]_$b[2]_$b[3]>Remove</a></td>";
+		echo "</tr>";
+		$flag++;
+		}
+		
+	}
+		
+		echo "</table>";
+	}
+	if($flag==1)	
+		echo "<br><center><div class='h'>No Records Found</div></center>";
+	echo "</div>		
+	<div class='col-sm-3'>	</div>
+	</div>";
 if(isset($_POST["out"]))
 {
 	$d=$_POST["dept"];
 	$x=$_POST["year"];
 	$y=$_POST["sec"];
 	$g=$_FILES['f']['name'];
-		$table_name=$x."_".$y;
+		$table_name=$x."_".$y."_".$d;
 	
 	$d=explode(".","$g");
 	
@@ -71,9 +123,9 @@ if($result) {
 		$sql1="insert into $table_name values('$ar[0]','$ar[1]','$ar[2]')";
 		$result=mysqli_query($con,$sql1);
 		if($result)
-		echo "<script>location.href='update_student.php?msg=Added Successfully'</script>";
+		echo "<script>location.href='add_class.php?msg=Added Successfully'</script>";
 		else
-		echo "<script>location.href='update_student.php?msg=Not Added'</script>";
+		echo "<script>location.href='add_class.php?msg=Not Added'</script>";
 	}
 	fclose($fp);
 }	
